07c074c1a579eaabb3389c6b7c4cc4e4f506bbb4
Johannes Rückert <4764281+anotherfrontendguy@users.noreply.github.com>
2022-11-14 12:36:38 +0100

    disable storybook telemetry

--- .storybook/main.js
+++ .storybook/main.js
@@ -1,16 +1,17 @@
-const path = require('path');
+const path = require("path");
 module.exports = {
-  "stories": [
-    "../src/**/*.stories.mdx",
-    "../src/**/*.stories.@(js|jsx|ts|tsx)"
-  ],
-  "addons": [
+  stories: ["../src/**/*.stories.mdx", "../src/**/*.stories.@(js|jsx|ts|tsx)"],
+  addons: [
     "@storybook/addon-links",
     "@storybook/addon-essentials",
-    "@storybook/addon-interactions"
+    "@storybook/addon-interactions",
   ],
-  "framework": {
-    "name": "@storybook/vue3-vite",
-    "options": {}
-  }
-}
+  framework: {
+    name: "@storybook/vue3-vite",
+    options: {},
+  },
+  core: {
+    disableTelemetry: true,
+  },
+};
+
